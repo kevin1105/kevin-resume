102350193cd4985e71b90277eb0ffafd385161cc
kevin1105 <31319917+kevin1105@users.noreply.github.com>
2022-12-08 06:35:57 +0000

    test

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// render app component in future
+const element = <h1> Hello World </h1>;
+ReactDOM.render(element, document.getElementById('root'));
